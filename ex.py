7abf9e8d42f0b7abc4e2f123af778284d93df401
Slava <shybakovslava@yandex.ru>
2020-03-30 21:10:04 +0500

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,16 +1,16 @@
 from nltk.tokenize import sent_tokenize
 
 patch_text = r'D:\slava\example.txt'
-path_write = r'D:\slava\writingf101.txt'
+path_write = r'D:\slava\writingf12.txt'
 
-txt = open(patch_text).read()
+txt = open(patch_text).read() # open text
 str2 = ''
-for c in txt:
+for c in txt: # delete all bad simbols in text
     if c not in ('I', 'V', '...', '[', ']', '(', ')', '…', '..', '–', '!..', '\r', ', ,', "»", "«"):
-        str2 += c
-str2 = str2.replace("\n", " ")
-sents = sent_tokenize(str2)
+        str2 += c 
+str2 = str2.replace("\n", " ") #replace \n -> ' '
+sents = sent_tokenize(str2) #tokenize
 f = open(path_write, 'w')
-for sents in sents:
-    sents = sents[:200]
-    f.write(sents + '\n')
+for sent in sents:# write to the file
+    if len(sent) <= 200:
+        f.write(sent + '\n')
